d8a33da50579fe83b7d9f9afc68f4ee9ce15a25a
mikaemul <mikael.multanen@metropolia.fi>
2019-11-04 18:27:47 +0200

    week2 update 4.11

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+'use strict';
+// catController
+const userModel = require('../models/userModel');
+
+const users = userModel.users;
+
+const user_list_get = (req, res) => {
+  res.json(users);
+};
+const user_get = (req, res) => {
+  const user = users.filter((henkilo) =>{
+    if(henkilo.id === req.params.id){
+      return henkilo;
+    }
+  });
+
+  res.json(user[0]);
+};
+
+module.exports = {
+  user_list_get, user_get,
+};
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,27 @@
 'use strict';
 // catRoute
+const express = require('express');
+const router =express.Router();
+const catController = require('../controllers/catController');
+
+router.get('/', catController.cat_list_get);
+router.get('/:id', catController.cat_get);
+
+router.get('/', (req, res) => {
+  res.send('With this endpoint you can get cats.');
+});
+router.post('/', (req, res) => {
+  res.send('With this endpoint you can add cats');
+});
+router.put('/', (req, res) =>  {
+  res.send('With this endpoint you can edit cats.');
+});
+router.delete('/', (req, res) => {
+  res.send('With this endpoint you can delete cats.');
+});
+router.get('/:id', function (req, res) {
+  const id = req.params.id;
+  res.send('You reqested a cat whose id is ' + id)
+});
+
+module.exports = router
