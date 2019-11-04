f10be731fc4feb2c291ce7e4d6ae34065c2f3ccc
mikaemul <mikael.multanen@metropolia.fi>
2019-11-04 15:22:20 +0200

    upd

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/cat', (req, res) => {
+  res.send('With this endpoint you can get cats.');
+});
+app.post('/cat', function (req, res) {
+  res.send('With this endpoint you can add cats')
+});
+app.put('/cat', function (req, res) {
+  res.send('With this endpoint you can edit cats.')
+});
+app.delete('/cat', function (req, res) {
+  res.send('With this endpoint you can delete cats.')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
